68610ab28b6ba36280f51c24194d96f0e6c28eb8
alphaprime8 <alphaprime8888@gmail.com>
2022-11-17 23:30:14 +0800

    success

--- programs/gdvesting/src/lib.rs
+++ programs/gdvesting/src/lib.rs
@@ -6,8 +6,9 @@ declare_id!("7w9oX4fSFFW9YK7iWYqBUzEwXJHa3UY3wP4y8HvpaU2s");
 pub mod gdvesting {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("VERSION 2");
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+        let a = 2;
+        msg!("VERSION {:?}", a);
         Ok(())
     }
 }
