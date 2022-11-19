c5af4e6c62860a3798b1e88568b4bbc732607a50
alphaprime8 <alphaprime8888@gmail.com>
2022-11-19 19:31:25 +0800

    partially drafted claim

--- programs/gdvesting/src/lib.rs
+++ programs/gdvesting/src/lib.rs
@@ -4,26 +4,106 @@ use anchor_spl::token::{self, Mint, Token, TokenAccount, Transfer};
 declare_id!("7w9oX4fSFFW9YK7iWYqBUzEwXJHa3UY3wP4y8HvpaU2s");
 
 // consts
+pub const MIN_ACCOUNT_LEN: usize = 9;
+const AUTH_PDA_SEED: &[u8] = b"auth_pda_seed";
 pub const VESTING_START_TIMESTAMP: u64 = 1685548800;
 
 #[program]
 pub mod gdvesting {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        investor: Pubkey,
+        vesting_rate: u64,
+        total_allocated_amount: u64,
+    ) -> Result<()> {
+        ctx.accounts.vesting_contract.investor = investor;
+        ctx.accounts.vesting_contract.vault = ctx.accounts.gigs_vault.key();
+        ctx.accounts.vesting_contract.vesting_rate = vesting_rate;
+        ctx.accounts.vesting_contract.total_allocated_amount = total_allocated_amount;
+        ctx.accounts.vesting_contract.claimed_amount = 0;
         Ok(())
     }
      pub fn claim(ctx: Context<Claim>) -> Result<()> {
+
+
+         /////////////////////////////////////////////
+         // TODO check start date
+         // TODO calculate amount vested
+
+         let mut contract = &mut ctx.accounts.vesting_contract;
+         let current_timestamp = Clock::get().unwrap().unix_timestamp as u64;
+         let claimable_amount = 0;
+
+         // TODO
+         /////////////////////////////////////////////
+
+
+         // transfer earned tokens
+        let signer_handle = &ctx.accounts.signer;
+        let tx_handle = ctx.accounts.receiver_gigs_ata.to_account_info();
+        let rx_handle = ctx.accounts.gigs_vault.to_account_info();
+        let token_program_acct_info = ctx.accounts.token_program.to_account_info();
+        transfer_tokens(signer_handle, tx_handle, rx_handle, token_program_acct_info, claimable_amount)?;
+
         Ok(())
     }
 
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+    init_if_needed,
+    seeds = [AUTH_PDA_SEED],
+    bump,
+    payer = signer,
+    space = MIN_ACCOUNT_LEN,
+    )]
+    pub auth_pda: Account<'info, AuthAccount>,
+    #[account(
+    init,
+    payer = signer,
+    space = 666, // TODO make this precise
+    )]
+    pub vesting_contract: Account<'info, VestingContract>,
+    pub gigs_mint: Account<'info, Mint>, // TODO add constraint for this
+    #[account(
+    init,
+    token::mint = gigs_mint,
+    token::authority = auth_pda,
+    payer = signer,
+    )]
+    pub gigs_vault: Account<'info, TokenAccount>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
 
 #[derive(Accounts)]
-pub struct Claim {}
+pub struct Claim<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+    mut,
+    seeds = [AUTH_PDA_SEED],
+    bump,
+    )]
+    pub auth_pda: Account<'info, AuthAccount>,
+    #[account(mut)]
+    pub vesting_contract: Account<'info, VestingContract>,
+    pub gigs_mint: Account<'info, Mint>, // TODO add constraint for this
+    #[account(mut)]
+    pub gigs_vault: Account<'info, TokenAccount>,
+    pub receiver_gigs_ata: Account<'info, TokenAccount>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>,
+}
 
 #[account]
 #[derive(Default)]
@@ -34,6 +114,10 @@ pub struct VestingContract {
     pub claimed_amount: u64,
     pub total_allocated_amount: u64,
 }
+
+#[account]
+#[derive(Default)]
+pub struct AuthAccount {}
 
 // utils
 pub fn transfer_tokens<'a>(
@@ -60,10 +144,8 @@ pub enum ErrorCode {
     InsufficientAmount,
     #[msg("Invalid Authorizer PDA.")]
     InvalidAuthPda,
-    #[msg("Proposal Not Active.")]
-    ProposalNotActive,
-    #[msg("Invalid Proposal ID.")]
-    InvalidProposalId,
+    #[msg("Vesting Start Date Not Reached.")]
+    VestingStartDateNotReached,
 }
 
 
