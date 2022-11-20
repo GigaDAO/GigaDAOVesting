5e1c7a80c0db406c3d9fa0b950c9db3f9b3952de
alphaprime8 <alphaprime8888@gmail.com>
2022-11-20 19:53:27 +0800

    initialized first investor

--- app/cli.ts
+++ app/cli.ts
@@ -1,10 +1,11 @@
 import * as anchor from "@project-serum/anchor";
+import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
 
 // env consts
 const IS_DEVNET = false;
 const LOCAL_KEYPAIR_FPATH = "/home/alphaprime8/.config/solana/id.json";
 const PROGRAM_ID = 'CA2CNT3eyyie4dKVos5ZoaePSAB2jCZaZRi7Zbdx1RNh'; // can also load from file as done with localKeypair below
-
+const AUTH_PDA_SEED = "auth_pda_seed";
 /*
 
  */
@@ -25,13 +26,48 @@ async function initProgram() {
     return new anchor.Program(idl, programId, provider);
 }
 
-async function initialize() {
+async function log_version() {
     const program = await initProgram();
     const tx = await program.methods.logVersion().rpc();
     console.log("Your transaction signature", tx);
 }
 
-initialize()
+async function initialize_contract() {
+    const program = await initProgram();
+    let [authPda, _] = await anchor.web3.PublicKey.findProgramAddress(
+        [Buffer.from(anchor.utils.bytes.utf8.encode(AUTH_PDA_SEED))],
+        program.programId
+    );
+
+    let vestingContract = anchor.web3.Keypair.generate();
+    let gigsVault = anchor.web3.Keypair.generate();
+
+    let gigsMint = new anchor.web3.PublicKey("9U8Bn6zAf6Wyp1YHdXtLyfbN7yMvdvW1qQY475iZ5ftZ");
+    let investor = new anchor.web3.PublicKey("413UGdeVRRpMbwM9LUdXQ8twHgPbbLCHZaqStQgueeTS");
+    let vesting_rate = new anchor.BN(6025);
+    let total_allocated_amount = new anchor.BN(380000000000);
+    let vesting_start_timestamp = new anchor.BN(1685548800);
+
+    // @ts-ignore
+    const tx = await program.methods.initialize(investor, vesting_rate, total_allocated_amount, vesting_start_timestamp)
+        .accounts({
+            signer: program.provider.publicKey,
+            authPda: authPda,
+            vestingContract: vestingContract.publicKey,
+            gigsMint: gigsMint,
+            gigsVault: gigsVault.publicKey,
+            tokenProgram: TOKEN_PROGRAM_ID,
+            systemProgram: anchor.web3.SystemProgram.programId,
+            rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+        })
+        .signers([vestingContract, gigsVault])
+        .rpc();
+
+    console.log("Your transaction signature", tx);
+
+}
+
+initialize_contract()
     .then(()=>{
         console.log("done")
     })
